ac74b45405ce87f7410d16febebcf69cd3ba7d3f
Jiang Chaoqun <jiangchaoqun.true@bytedance.com>
2023-04-18 00:47:09 +0800

    feat: 用黑客的方式修复 NGINX 反向代理后导致 Destination 校验不通过的问题
    Change-Id: Ic13371d4825a2ba285b755753accb7b0ab4a2cdb

--- api/webdav/webdav.go
+++ api/webdav/webdav.go
@@ -3,12 +3,16 @@
 package webdav
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/mcoder2014/home_server/api/middleware"
 	"github.com/mcoder2014/home_server/config"
 	"github.com/mcoder2014/home_server/data"
+	"github.com/mcoder2014/home_server/utils/log"
 	"golang.org/x/net/webdav"
 )
 
@@ -24,6 +28,7 @@ func InitRouter() error {
 		Prefix:     "/webdav/",
 		FileSystem: webdav.Dir(sharePath),
 		LockSystem: webdav.NewMemLS(),
+		Logger:     Logger,
 	}
 
 	webDAVPath := "/webdav/*path"
@@ -44,5 +49,49 @@ func InitRouter() error {
 }
 
 func WebDAV(c *gin.Context) {
+	// 用比较黑客的方式规避 Nginx 代理后 URL 与 Host 不一致的问题
+	logID := c.GetString(log.LogIDKey)
+	c.Request.Header.Set(log.LogIDKey, logID)
+	c.Writer.Header().Set(log.LogIDKey, logID)
+
+	destination := c.Request.Header.Get("Destination")
+	if len(destination) > 0 {
+		idx := strings.Index(destination, "/webdav/")
+		if idx >= 0 {
+			destination = "http://" + c.Request.Host + destination[idx:]
+		}
+		c.Request.Header.Set("Destination", destination)
+	}
 	rawHandler.ServeHTTP(c.Writer, c.Request)
+}
+
+func Logger(req *http.Request, err error) {
+	url := req.URL.Path
+	headers := req.Header
+	logID := headers.Get(log.LogIDKey)
+	ctx := context.WithValue(context.Background(), log.LogIDKey, logID)
+
+	if err != nil {
+		var sb strings.Builder
+		sb.WriteString("webdav error: ")
+		sb.WriteString(err.Error())
+		sb.WriteString("\n")
+		sb.WriteString("Host: ")
+		sb.WriteString(req.Host)
+		sb.WriteString("\n")
+		sb.WriteString("url: ")
+		sb.WriteString(url)
+		sb.WriteString("\n")
+		sb.WriteString("headers: ")
+		for k, v := range headers {
+			sb.WriteString("\t")
+			sb.WriteString(k)
+			sb.WriteString(": ")
+			sb.WriteString(v[0])
+			sb.WriteString("\n")
+		}
+		log.Ctx(ctx).Errorf("[WebDAV] [%v] %v", req.Method, sb.String())
+	} else {
+		log.Ctx(ctx).Infof("[WebDAV] [%v] %v", req.Method, url)
+	}
 }
